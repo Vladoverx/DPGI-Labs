c2eedb2b40329f391c9873b4253208d806e833b1
Vladislav Bugaichenko <92445789+Vladoverx@users.noreply.github.com>
2024-03-01 12:57:57 +0200

    add Lab1 solution

---
+++ Lab1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Hello_World
+{
+
+    /// <summary>
+    /// Represents a class for displaying greetings.
+    /// </summary>
+    public class Hello
+    {
+        /// <summary>
+        /// Displays the "Hello, World!" greeting.
+        /// </summary>
+        public static void DisplayGreeting()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    /// <summary>
+    /// The main class of the program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The entry point of the program.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            ArgumentNullException.ThrowIfNull(args);
+            // Creates an instance of the Hello class.
+            Hello hello = new();
+            // Calls the method to display the greeting.
+            Hello.DisplayGreeting();
+        }
+    }
+}
---
+++ Lab1/readme.txt
@@ -0,0 +1,3 @@
+Initial project for Graphic Interfaces context
+
+27.02.2024 created first version of program
